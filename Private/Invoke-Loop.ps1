7635aee325090eadc49218814664861b71cf89f5
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-10-16 16:31:40 -0700

    Update Invoke-Loop.ps1
    Condensed some of the code in Invoke-Loop after previous bugfix

--- Private/Invoke-Loop.ps1
+++ Private/Invoke-Loop.ps1
@@ -26,66 +26,48 @@ function Invoke-Loop {
         [string] $Detailed
     )
     process {
-        # Request identifiers
-        $Loop = & $Command @Param
-
         if ($Detailed) {
-            # Add parameter to target identifiers to command string
-            $DetailParam = @{
-                $Detailed = $Loop.resources
-            }
             # Output detail about identifiers
+            $DetailParam = @{
+                $Detailed = (& $Command @Param -OutVariable Loop).resources
+            }
             & $Command @DetailParam
         } else {
-            # Output identifiers
-            $Loop
+            # Request identifiers
+            & $Command @Param -OutVariable Loop
         }
         if ($Loop.resources -and (($Loop.resources.count -lt $Loop.meta.pagination.total) -or
         $Loop.meta.pagination.next_page)) {
-
-            Write-Verbose ("[$($MyInvocation.MyCommand.Name)] $($Loop.meta.pagination.total) total results")
-
             # Repeat requests if result count is less than total results, or until 'next_page' disappears
             for ($i = $Loop.resources.count; (($i -lt $Loop.meta.pagination.total) -or
             ($Loop.meta.pagination.next_page)); $i += $Loop.resources.count) {
 
-                # Update position based on pagination results
-                if ($Loop.meta) {
-                    if ($Loop.meta.pagination.after) {
-                        # token-based after
-                        $Param['After'] = $Loop.meta.pagination.after
-                    } else {
-                        $Param['Offset'] = if ($Loop.meta.pagination.next_page) {
-                            # uoffset value when 'next_page' is present
-                            $Loop.meta.pagination.offset
-                        } elseif ($Loop.meta.pagination.offset -match '^\d{1,}$') {
-                            # current count for integer-offset
-                            $i
-                        } else {
-                            # token-based offset
-                            $Loop.meta.pagination.offset
-                        }
-                    }
-                    Write-Verbose "[$($MyInvocation.MyCommand.Name)] received $i of $($Loop.meta.pagination.total)"
+                Write-Verbose "[$($MyInvocation.MyCommand.Name)] $i of $($Loop.meta.pagination.total)"
 
-                    # Request identifiers
-                    $Loop = & $Command @Param
-
-                    if ($Detailed) {
-                        # Add parameter to target identifiers to command string
-                        $DetailParam = @{
-                            $Detailed = $Loop.resources
-                        }
-                        # Output detail about identifiers
-                        & $Command @DetailParam
-                    } else {
-                        # Output identifiers
-                        $Loop
-                    }
+                if ($Loop.meta.pagination.after) {
+                    # token-based after
+                    $Param['After'] = $Loop.meta.pagination.after
                 } else {
-                    # Output error
-                    $Loop
-                    break
+                    $Param['Offset'] = if ($Loop.meta.pagination.next_page) {
+                        # uoffset value when 'next_page' is present
+                        $Loop.meta.pagination.offset
+                    } elseif ($Loop.meta.pagination.offset -match '^\d{1,}$') {
+                        # current count for integer-offset
+                        $i
+                    } else {
+                        # token-based offset
+                        $Loop.meta.pagination.offset
+                    }
+                }
+                if ($Detailed) {
+                    # Output detail about identifiers
+                    $DetailParam = @{
+                        $Detailed = (& $Command @Param -OutVariable Loop).resources
+                    }
+                    & $Command @DetailParam
+                } else {
+                    # Request identifiers
+                    & $Command @Param -OutVariable Loop
                 }
             }
         }
