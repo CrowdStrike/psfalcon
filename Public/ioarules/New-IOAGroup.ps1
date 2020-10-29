4e796d2f83db28b14a0191fc15bfd73e4d05204c
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-10-29 16:08:08 -0700

    Update New-IOAGroup.ps1

--- Public/ioarules/New-IOAGroup.ps1
+++ Public/ioarules/New-IOAGroup.ps1
@@ -1,1 +1,29 @@
-# ioarules/create-rule-group
+function New-IOAGroup {
+<#
+.SYNOPSIS
+    Create a custom Indicator of Attack rule group
+.DESCRIPTION
+    Additional information is available with the -Help parameter
+.LINK
+    https://github.com/CrowdStrike/psfalcon
+#>
+    [CmdletBinding(DefaultParameterSetName = 'ioarules/create-rule-group')]
+    [OutputType()]
+    param()
+    DynamicParam {
+        # Endpoint(s) used by function
+        $Endpoints = @('ioarules/create-rule-group')
+
+        # Create runtime dictionary
+        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+    }
+    process {
+        if ($PSBoundParameters.Help) {
+            # Output help information
+            Get-DynamicHelp $MyInvocation.MyCommand.Name
+        } else {
+            # Evaluate input and make request
+            Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
+        }
+    }
+}
