885c370185cc0b1317e705da6070cc15988c8cab
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-10-29 16:08:05 -0700

    Update Get-IOAPlatform.ps1

--- Public/ioarules/Get-IOAPlatform.ps1
+++ Public/ioarules/Get-IOAPlatform.ps1
@@ -1,1 +1,40 @@
-# ioarules/get-platforms, ioarules/query-platforms
+function Get-IOAPlatform {
+<#
+.SYNOPSIS
+    Search for available platforms for use with custom Indicators of Attack
+.DESCRIPTION
+    Additional information is available with the -Help parameter
+.LINK
+    https://github.com/CrowdStrike/psfalcon
+#>
+    [CmdletBinding(DefaultParameterSetName = 'ioarules/query-platforms')]
+    [OutputType()]
+    param()
+    DynamicParam {
+        # Endpoint(s) used by function
+        $Endpoints = @('ioarules/query-platforms', 'ioarules/get-platforms')
+
+        # Create runtime dictionary
+        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+    }
+    process {
+        if ($PSBoundParameters.Help) {
+            # Output help information
+            Get-DynamicHelp $MyInvocation.MyCommand.Name
+        } else {
+            # Evaluate input and make request
+            $Param = @{
+                Command = $MyInvocation.MyCommand.Name
+                Query = $Endpoints[0]
+                Entity = $Endpoints[1]
+                Dynamic = $Dynamic
+            }
+            switch ($PSBoundParameters.Keys) {
+                'Detailed' {
+                    $Param['Detailed'] = 'PlatformIds'
+                }
+            }
+            Invoke-Request @Param
+        }
+    }
+}
