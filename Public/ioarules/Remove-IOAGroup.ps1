b1c8a38e75d1d4f6e2280212d9779ff4368af27d
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-10-29 16:08:10 -0700

    Update Remove-IOAGroup.ps1

--- Public/ioarules/Remove-IOAGroup.ps1
+++ Public/ioarules/Remove-IOAGroup.ps1
@@ -1,1 +1,29 @@
-# ioarules/delete-rule-groups
+function Remove-IOAGroup {
+<#
+.SYNOPSIS
+    Remove custom Indicator of Attack rule groups
+.DESCRIPTION
+    Additional information is available with the -Help parameter
+.LINK
+    https://github.com/CrowdStrike/psfalcon
+#>
+    [CmdletBinding(DefaultParameterSetName = 'ioarules/delete-rule-groups')]
+    [OutputType()]
+    param()
+    DynamicParam {
+        # Endpoint(s) used by function
+        $Endpoints = @('ioarules/delete-rule-groups')
+
+        # Create runtime dictionary
+        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+    }
+    process {
+        if ($PSBoundParameters.Help) {
+            # Output help information
+            Get-DynamicHelp $MyInvocation.MyCommand.Name
+        } else {
+            # Evaluate input and make request
+            Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
+        }
+    }
+}
