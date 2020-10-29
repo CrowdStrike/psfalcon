dd514b4fe8340923b6b6dce23923018b162034f5
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-10-29 16:08:11 -0700

    Update Remove-IOARule.ps1

--- Public/ioarules/Remove-IOARule.ps1
+++ Public/ioarules/Remove-IOARule.ps1
@@ -1,1 +1,29 @@
-# ioarules/delete-rules
+function Remove-IOARule {
+<#
+.SYNOPSIS
+    Remove custom Indicator of Attack rules
+.DESCRIPTION
+    Additional information is available with the -Help parameter
+.LINK
+    https://github.com/CrowdStrike/psfalcon
+#>
+    [CmdletBinding(DefaultParameterSetName = 'ioarules/delete-rules')]
+    [OutputType()]
+    param()
+    DynamicParam {
+        # Endpoint(s) used by function
+        $Endpoints = @('ioarules/delete-rules')
+
+        # Create runtime dictionary
+        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+    }
+    process {
+        if ($PSBoundParameters.Help) {
+            # Output help information
+            Get-DynamicHelp $MyInvocation.MyCommand.Name
+        } else {
+            # Evaluate input and make request
+            Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
+        }
+    }
+}
