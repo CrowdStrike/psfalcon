af23d1b2e1d5c07169e63cbba0f6515704af67fb
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-03 17:21:08 -0800

    Update Edit-IOARule.ps1

--- Public/ioarules/Edit-IOARule.ps1
+++ Public/ioarules/Edit-IOARule.ps1
@@ -1,1 +1,35 @@
-# ioarules/update-rules
+function Edit-IOARule {
+<#
+.SYNOPSIS
+    Modify a custom Indicator of Attack rule
+.DESCRIPTION
+    Additional information is available with the -Help parameter
+.LINK
+    https://github.com/CrowdStrike/psfalcon
+#>
+    [CmdletBinding(DefaultParameterSetName = 'ioarules/update-rules')]
+    [OutputType()]
+    param()
+    DynamicParam {
+        # Endpoint(s) used by function
+        $Endpoints = @('ioarules/update-rules')
+
+        # Create runtime dictionary
+        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+    }
+    process {
+        if ($PSBoundParameters.Help) {
+            # Output help information
+            Get-DynamicHelp $MyInvocation.MyCommand.Name
+        } else {
+            foreach ($Param in (Get-Param $Endpoints[0] $Dynamic)) {
+                # Add Header parameter with api-client-id
+                $Param['Header'] = @{
+                    'X-CS-USERNAME' = "api-client-id:$($Falcon.id)"
+                }
+                # Make request
+                Invoke-Endpoint @Param
+            }
+        }
+    }
+}
