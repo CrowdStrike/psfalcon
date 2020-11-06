6cb453197417d3f804e297fb4544c24d6aa0e059
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-06 15:52:50 -0800

    PSScriptAnalyzer CodeFormatting

--- Public/samples/Remove-Sample.ps1
+++ Public/samples/Remove-Sample.ps1
@@ -1,28 +1,24 @@
-function Remove-Sample {
-<#
-.SYNOPSIS
-    Remove a sample file
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Remove-Sample {
+    <#
+    .SYNOPSIS
+        Remove a sample file
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'samples/DeleteSampleV2')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('samples/DeleteSampleV2')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
-            # Evaluate input and make request
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
         }
     }
--- Public/samples/Send-Sample.ps1
+++ Public/samples/Send-Sample.ps1
@@ -1,37 +1,32 @@
-function Send-Sample {
-<#
-.SYNOPSIS
-    Upload a sample for submission
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Send-Sample {
+    <#
+    .SYNOPSIS
+        Upload a sample for submission
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'samples/UploadSampleV2')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('samples/UploadSampleV2')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     begin {
         if (-not $Dynamic.FileName.Value) {
-            # Capture filename from path
             $Dynamic.FileName.Value = "$([System.IO.Path]::GetFileName($Dynamic.Path.Value))"
         }
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } elseif (-not(Test-Path $Dynamic.Path.Value)) {
-            # Output exception for invalid file path
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        elseif (-not(Test-Path $Dynamic.Path.Value)) {
             throw "Cannot find path '$($Dynamic.Path.Value)' because it does not exist."
-        } else {
-            # Evaluate input and make request
+        }
+        else {
             Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
         }
     }
