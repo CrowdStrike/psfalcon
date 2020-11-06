9691928775c84c167d7c85974960df1d74878597
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-06 15:09:51 -0800

    PSScriptAnalyzer CodeFormatting

--- Public/oauth2/Request-Token.ps1
+++ Public/oauth2/Request-Token.ps1
@@ -1,29 +1,25 @@
-function Request-Token {
-<#
-.SYNOPSIS
-    Generate an OAuth2 access token
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Request-Token {
+    <#
+    .SYNOPSIS
+        Generate an OAuth2 access token
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'oauth2/oauth2AccessToken')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('oauth2/oauth2AccessToken')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             if ($PSBoundParameters.Cloud) {
-                # Set hostname
                 $Falcon.Hostname = switch ($PSBoundParameters.Cloud) {
                     'eu-1' { 'https://api.eu-1.crowdstrike.com' }
                     'us-gov-1' { 'https://api.laggar.gcw.crowdstrike.com' }
@@ -34,64 +30,50 @@ function Request-Token {
             }
             if (-not($Dynamic.Id.Value)) {
                 if ($Falcon.Id) {
-                    # Collect saved Id
                     $Dynamic.Id.Value = $Falcon.Id
-                } else {
-                    # Force input of Id
+                }
+                else {
                     $Dynamic.Id.Value = Read-Host "Id"
                 }
             }
             if (-not($Dynamic.Secret.Value)) {
                 if ($Falcon.Secret) {
-                    # Collect saved Secret
                     $Dynamic.Secret.Value = $Falcon.Secret
-                } else {
-                    # Force input of Secret
+                }
+                else {
                     $Dynamic.Secret.Value = Read-Host "Secret"
                 }
             }
             Write-Debug "[$($MyInvocation.MyCommand.Name)] id: $($Dynamic.Id.Value)"
-
             if ((-not($Dynamic.CID.Value)) -and $Falcon.CID) {
-                # Collect saved CID
                 $Dynamic.CID.Value = $Falcon.CID
             }
-            # Base parameters
             $Param = @{
                 Endpoint = $Endpoints[0]
-                Body = "client_id=$($Dynamic.Id.Value)&client_secret=$($Dynamic.Secret.Value)"
+                Body     = "client_id=$($Dynamic.Id.Value)&client_secret=$($Dynamic.Secret.Value)"
             }
             if ($Dynamic.CID.Value) {
                 $Param.Body += "&member_cid=$($Dynamic.CID.Value)"
-
                 Write-Debug "[$($MyInvocation.MyCommand.Name)] cid: $($Dynamic.CID.Value)"
             }
-            # Make request
             $Request = Invoke-Endpoint @Param
-
             if ($Request.access_token) {
-                # Save credentials
                 $Falcon.Id = $Dynamic.Id.Value
                 $Falcon.Secret = $Dynamic.Secret.Value
-
                 if ($Dynamic.CID.Value) {
-                    # Save CID
                     $Falcon.CID = $Dynamic.CID.Value
                 }
-                # Save token and expiration
                 Write-Verbose "[$($MyInvocation.MyCommand.Name)] token generated"
-
                 $Falcon.Expires = (Get-Date).AddSeconds($Request.expires_in)
                 $Falcon.Token = "$($Request.token_type) $($Request.access_token)"
-            } else {
-                # Clear cached authentication information
+            }
+            else {
                 @('id', 'secret', 'CID', 'token').foreach{
                     if ($Falcon.$_) {
                         $Falcon.$_ = $null
                         Write-Debug "[$($MyInvocation.MyCommand.Name)] cleared $_"
                     }
                 }
-                # Output error
                 $Request
             }
         }
--- Public/oauth2/Revoke-Token.ps1
+++ Public/oauth2/Revoke-Token.ps1
@@ -1,5 +1,5 @@
-function Revoke-Token {
-<#
+﻿function Revoke-Token {
+    <#
 .SYNOPSIS
     Revoke your current OAuth2 access token before the end of its standard lifespan
 .LINK
@@ -9,31 +9,23 @@ function Revoke-Token {
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('oauth2/oauth2RevokeToken')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             if (-not $Falcon.Token) {
-                # Output exception if a token is not present
                 throw "No token available to be revoked"
             }
-            # Base request parameters
             $Param = @{
                 Endpoint = $Endpoints[0]
-                Body = "token=$($Falcon.token -replace 'bearer ', '')"
+                Body     = "token=$($Falcon.token -replace 'bearer ', '')"
             }
-            # Make request
             $Request = Invoke-Endpoint @Param
-
             if ($Request.meta) {
-                # Clear cached token and credential information
                 @('id', 'secret', 'CID', 'token').foreach{
                     if ($Falcon.$_) {
                         $Falcon.$_ = $null
@@ -42,7 +34,6 @@ function Revoke-Token {
                 }
                 $Falcon.Expires = Get-Date
             }
-            # Output result
             $Request
         }
     }
