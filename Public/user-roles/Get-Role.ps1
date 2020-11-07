a61fd5cc7448e4c35ab08e2e3cde80d115c7b7d1
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-06 16:14:13 -0800

    PSScriptAnalyzer CodeFormatting

--- Public/user-roles/Get-Role.ps1
+++ Public/user-roles/Get-Role.ps1
@@ -1,32 +1,28 @@
-function Get-Role {
-<#
-.SYNOPSIS
-    Search for user roles, or roles assigned to a specific user
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Get-Role {
+    <#
+    .SYNOPSIS
+        Search for user roles, or roles assigned to a specific user
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'user-roles/GetAvailableRoleIds')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('user-roles/GetAvailableRoleIds', 'user-roles/GetRoles', 'user-roles/GetUserRoleIds')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
-            # Evaluate input and make request
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             $Param = @{
                 Command = $MyInvocation.MyCommand.Name
-                Query = $PSCmdlet.ParameterSetName
-                Entity = $Endpoints[1]
+                Query   = $PSCmdlet.ParameterSetName
+                Entity  = $Endpoints[1]
                 Dynamic = $Dynamic
             }
             switch ($PSBoundParameters.Keys) {
--- Public/user-roles/Remove-Role.ps1
+++ Public/user-roles/Remove-Role.ps1
@@ -1,28 +1,24 @@
-function Remove-Role {
-<#
-.SYNOPSIS
-    Remove one or more roles from a user
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Remove-Role {
+    <#
+    .SYNOPSIS
+        Remove one or more roles from a user
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'user-roles/RevokeUserRoleIds')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('user-roles/RevokeUserRoleIds')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
-            # Evaluate input and make request
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
         }
     }
