aaa3a7d727614189ced93209b1dbdae6f5b3a36f
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-06 16:13:17 -0800

    PSScriptAnalyzer CodeFormatting

--- Public/spotlight/Get-Remediation.ps1
+++ Public/spotlight/Get-Remediation.ps1
@@ -1,28 +1,24 @@
-function Get-Remediation {
-<#
-.SYNOPSIS
-    Get information about remediations
-.DESCRIPTION
-    Additional information is available with the -Help parameter
-.LINK
-    https://github.com/CrowdStrike/psfalcon
-#>
+﻿function Get-Remediation {
+    <#
+    .SYNOPSIS
+        Get information about remediations
+    .DESCRIPTION
+        Additional information is available with the -Help parameter
+    .LINK
+        https://github.com/CrowdStrike/psfalcon
+    #>
     [CmdletBinding(DefaultParameterSetName = 'spotlight/getRemediations')]
     [OutputType()]
     param()
     DynamicParam {
-        # Endpoint(s) used by function
         $Endpoints = @('spotlight/getRemediations')
-
-        # Create runtime dictionary
-        return (Get-Dictionary $Endpoints -OutVariable Dynamic)
+        return (Get-Dictionary -Endpoints $Endpoints -OutVariable Dynamic)
     }
     process {
         if ($PSBoundParameters.Help) {
-            # Output help information
-            Get-DynamicHelp $MyInvocation.MyCommand.Name
-        } else {
-            # Evaluate input and make request
+            Get-DynamicHelp -Command $MyInvocation.MyCommand.Name
+        }
+        else {
             Invoke-Request -Query $Endpoints[0] -Dynamic $Dynamic
         }
     }
