22634093f616d06d4b52da35b6490821c350c069
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-09 11:51:14 -0800

    Update Request-Token.ps1
    Modified switch statement to ensure existing hostname value is used (if present) when making token request

--- Public/oauth2/Request-Token.ps1
+++ Public/oauth2/Request-Token.ps1
@@ -20,11 +20,26 @@
         }
         else {
             $Falcon.Hostname = switch ($PSBoundParameters.Cloud) {
-                'eu-1' { 'https://api.eu-1.crowdstrike.com' }
-                'us-gov-1' { 'https://api.laggar.gcw.crowdstrike.com' }
-                'us-1' { 'https://api.crowdstrike.com' }
-                'us-2' { 'https://api.us-2.crowdstrike.com' }
-                default { 'https://api.crowdstrike.com' }
+                'eu-1' {
+                    'https://api.eu-1.crowdstrike.com'
+                }
+                'us-gov-1' {
+                    'https://api.laggar.gcw.crowdstrike.com'
+                }
+                'us-1' {
+                    'https://api.crowdstrike.com'
+                }
+                'us-2' {
+                    'https://api.us-2.crowdstrike.com'
+                }
+                default {
+                    if ($Falcon.Hostname) {
+                        $Falcon.Hostname
+                    }
+                    else {
+                        'https://api.crowdstrike.com'
+                    }
+                }
             }
             @('Id', 'Secret', 'CID') | ForEach-Object {
                 if (-not($PSBoundParameters.$_)) {
