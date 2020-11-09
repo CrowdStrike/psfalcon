eeb5f23852bc3828877d1e23d9ce1aa577567768
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-09 10:58:15 -0800

    Update Format-Header.ps1
    Added filter to remove "Authorization" from Write-Debug output

--- Private/Format-Header.ps1
+++ Private/Format-Header.ps1
@@ -48,6 +48,7 @@
         if ($Authorization) {
             $Request.Headers.Add('Authorization', $Authorization)
         }
-        Write-Debug "[$($MyInvocation.MyCommand.Name)] $(ConvertTo-Json $Request.Headers)"
+        Write-Debug ("[$($MyInvocation.MyCommand.Name)] " +
+            "$(ConvertTo-Json ($Request.Headers | Where-Object { $_.Key -NE 'Authorization' }))")
     }
 }
