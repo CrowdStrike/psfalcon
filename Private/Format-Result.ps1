20bd411732ec29048731cba9e5c0f047a98aed20
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-25 10:27:02 -0800

    Bugfix for PowerShell 5.1 output
    Select-Object -ExcludeProperty didn't work as expected in PowerShell 5.1. Changed the logic to support both 5.1 and Core.
    
    Added try/catch loop.

--- Private/Format-Result.ps1
+++ Private/Format-Result.ps1
@@ -24,38 +24,45 @@
         }
     }
     process {
-        if ($Response.Result.Content -match '^<') {
-            $HTML = ($Response.Result.Content).ReadAsStringAsync().Result
-        }
-        elseif ($Response.Result.Content) {
-            $Json = ConvertFrom-Json ($Response.Result.Content).ReadAsStringAsync().Result
-        }
-        if ($Json) {
-            Read-Meta -Object $Json -Endpoint $Endpoint
-            $Output = $Json | Select-Object -ExcludeProperty Meta
-            $Populated = ($Output.PSObject.Properties).foreach{
-                if ($_.Value) {
-                    $_.Name
+        try {
+            if ($Response.Result.Content -match '^<') {
+                $HTML = ($Response.Result.Content).ReadAsStringAsync().Result
+            }
+            elseif ($Response.Result.Content) {
+                $Json = ConvertFrom-Json ($Response.Result.Content).ReadAsStringAsync().Result
+            }
+            if ($Json) {
+                Read-Meta -Object $Json -Endpoint $Endpoint
+                $Populated = ($Json.PSObject.Properties).foreach{
+                    if ($_.Value -and ($_.Name -ne 'meta')) {
+                        $_.Name
+                    }
                 }
-            }
-            if ($Populated.count -eq 1) {
-                $Output.($Populated[0])
-            }
-            else {
-                if ($Definition) {
-                    $Output.PSObject.TypeNames.Insert(0,$Definition)
+                $Output = if ($Populated.count -eq 1) {
+                    if ($Populated[0] -eq 'errors') {
+                        throw "$($Json.errors.code): $($Json.errors.message)"
+                    }
+                    else {
+                        $Json.($Populated[0])
+                    }
+                }
+                else {
+                    $Json
                 }
                 $Output
             }
+            elseif ($HTML) {
+                $HTML
+            }
+            elseif ($Response.Result) {
+                $Response.Result.EnsureSuccessStatusCode()
+            }
+            else {
+                $Response
+            }
         }
-        elseif ($HTML) {
-            $HTML
-        }
-        elseif ($Response.Result) {
-            $Response.Result.EnsureSuccessStatusCode()
-        }
-        else {
-            $Response
+        catch {
+            Write-Error $_.Exception.Message
         }
     }
 }
