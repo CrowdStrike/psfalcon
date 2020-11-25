8e2bb81813f7c3204014031af7e72748ae13b770
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-24 16:33:19 -0800

    Updated for Read-Meta
    Updated to work with the removal of $_.meta in responses and the creation of $Script:Meta by Read-Meta

--- Private/Invoke-Loop.ps1
+++ Private/Invoke-Loop.ps1
@@ -32,35 +32,38 @@
         else {
             $Loop
         }
-        if ($Loop.resources -and (($Loop.resources.count -lt $Loop.meta.pagination.total) -or
-            $Loop.meta.pagination.next_page)) {
-            for ($i = $Loop.resources.count; (($i -lt $Loop.meta.pagination.total) -or
-                ($Loop.meta.pagination.next_page)); $i += $Loop.resources.count) {
-                Write-Verbose "[$($MyInvocation.MyCommand.Name)] $i of $($Loop.meta.pagination.total)"
-                if ($Loop.meta.pagination.after) {
-                    $Param['After'] = $Loop.meta.pagination.after
+        if ($Loop -and (($Loop.count -lt $Meta.pagination.total) -or
+        $Meta.pagination.next_page)) {
+            for ($i = $Loop.count; (($i -lt $Meta.pagination.total) -or
+                ($Meta.pagination.next_page)); $i += $Loop.count) {
+                Write-Verbose "[$($MyInvocation.MyCommand.Name)] $i of $($Meta.pagination.total)"
+                if ($Meta.pagination.after) {
+                    $Param['After'] = $Meta.pagination.after
                 }
                 else {
-                    $Param['Offset'] = if ($Loop.meta.pagination.next_page) {
-                        $Loop.meta.pagination.offset
+                    $Param['Offset'] = if ($Meta.pagination.next_page) {
+                        $Meta.pagination.offset
                     }
-                    elseif ($Loop.meta.pagination.offset -match '^\d{1,}$') {
+                    elseif ($Meta.pagination.offset -match '^\d{1,}$') {
                         $i
                     }
                     else {
-                        $Loop.meta.pagination.offset
+                        $Meta.pagination.offset
                     }
                 }
                 $Loop = & $Command @Param
-                if ($Loop.resources -and $Detailed -and $Detailed -ne 'Combined') {
+                if ($Loop -and $Detailed -and $Detailed -ne 'Combined') {
                     $DetailParam = @{
-                        $Detailed = $Loop.resources
+                        $Detailed = $Loop
                     }
                     & $Command @DetailParam
                 }
                 else {
                     $Loop
                 }
+            }
+            if ($Script:Meta) {
+                Remove-Variable -Name Meta -Scope Script
             }
         }
     }
