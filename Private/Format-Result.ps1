cbf484b5c3ebd0f694b7f95f9372fb48eafc1cf0
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-11-24 16:34:16 -0800

    Update for Read-Meta
    Updated for Read-Meta and added in basic logic (tested with simple "id only" functions so far) for displaying resources, errors, or both if they're not equal to $null

--- Private/Format-Result.ps1
+++ Private/Format-Result.ps1
@@ -16,21 +16,40 @@
         [Parameter(Mandatory = $true)]
         [string] $Endpoint
     )
+    begin {
+        $StatusCode = $Response.Result.StatusCode.GetHashCode()
+        if ($StatusCode) {
+            $Definition = $Falcon.Response($Endpoint, $StatusCode)
+            Write-Verbose ("[$($MyInvocation.MyCommand.Name)] $($StatusCode): $Definition")
+        }
+    }
     process {
-        $Output = if ($Response.Result.Content) {
-            if ($Response.Result.Content -match '^<') {
-                ($Response.Result.Content).ReadAsStringAsync().Result
+        if ($Response.Result.Content -match '^<') {
+            $HTML = ($Response.Result.Content).ReadAsStringAsync().Result
+        }
+        elseif ($Response.Result.Content) {
+            $Json = ConvertFrom-Json ($Response.Result.Content).ReadAsStringAsync().Result
+        }
+        if ($Json) {
+            Read-Meta -Object $Json -Endpoint $Endpoint
+            $Output = $Json | Select-Object -ExcludeProperty Meta
+            $Populated = ($Output.PSObject.Properties).foreach{
+                if ($_.Value) {
+                    $_.Name
+                }
+            }
+            if ($Populated.count -eq 1) {
+                $Output.($Populated[0])
             }
             else {
-                ConvertFrom-Json ($Response.Result.Content).ReadAsStringAsync().Result
+                if ($Definition) {
+                    $Output.PSObject.TypeNames.Insert(0,$Definition)
+                }
+                $Output
             }
         }
-        if ($Output) {
-            $TypeName = $Falcon.Endpoint($Endpoint).Responses.($Response.result.StatusCode.GetHashCode())
-            if ($TypeName) {
-                $Output.PSObject.TypeNames.Insert(0, "$TypeName")
-            }
-            $Output
+        elseif ($HTML) {
+            $HTML
         }
         elseif ($Response.Result) {
             $Response.Result.EnsureSuccessStatusCode()
