8a739045fc16242c5aa2591691e3aa0407316477
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-12-04 14:15:40 -0800

    Update Find-Duplicate.ps1
    Updated Find-Duplicate after testing. Command only supports detailed 'Hosts' results and requires the 'device_id', 'hostname', 'first_seen', 'last_seen' and 'mac_address' fields.

--- Public/scripts/Find-Duplicate.ps1
+++ Public/scripts/Find-Duplicate.ps1
@@ -1,9 +1,9 @@
 function Find-Duplicate {
     <#
     .SYNOPSIS
-        Lists potential duplicates from a detailed API result
+        Lists potential duplicates from detailed 'Host' results
     .PARAMETER HOSTS
-        A detailed 'Hosts' API result
+        Array of detailed 'Host' results
     .LINK
         https://github.com/crowdstrike/psfalcon
     #>
@@ -15,23 +15,33 @@ function Find-Duplicate {
             Mandatory = $true)]
         [array] $Hosts
     )
-    process {
-        if ($Hosts) {
-            $InputData = $Hosts | Select-Object device_id, hostname, first_seen, last_seen, mac_address |
-            Sort-Object last_seen
-            $Criteria = 'hostname'
-        }
-        try {
-            $Duplicates = if ($InputData -and $Criteria) {
-                ($InputData | Group-Object $Criteria | Where-Object { $_.count -gt 1 } |
-                Select-Object -ExpandProperty Group | Group-Object $Criteria) | ForEach-Object { $_.Group |
-                Select-Object -First (($_.count) - 1) }
+    begin {
+        $Criteria = @('device_id', 'hostname', 'first_seen', 'last_seen', 'mac_address')
+        $InputFields = ($Hosts | Get-Member -MemberType NoteProperty).Name
+        function Group-Selection ($Selection, $Criteria) {
+            $Selection | Group-Object $Criteria | Where-Object { $_.count -gt 1 } |
+            Select-Object -ExpandProperty Group | Group-Object $Criteria | ForEach-Object {
+                $_.Group | Select-Object -First (($_.count) - 1)
             }
+        }
+    }
+    process {
+        try {
+            ($Criteria).foreach{
+                if ($InputFields -notcontains $_) {
+                    throw "Input object does not contain '$_' field"
+                }
+            }
+            $Param = @{
+                Selection = $Hosts | Select-Object $Criteria
+                Criteria = 'hostname'
+            }
+            $Duplicates = Group-Selection @Param 
             if ($Duplicates) {
                 $Duplicates
             }
             else {
-                Write-Host "No duplicates found"
+                Write-Warning "No duplicates found"
             }
         }
         catch {
