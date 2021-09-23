226b1f427575917264700c74ff7c69d842687976
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-09-23 10:35:50 -0700

    v2.1.4

--- Public/identity-graphql.ps1
+++ Public/identity-graphql.ps1
@@ -1,26 +1,4 @@
 function Invoke-FalconIdentityGraph {
-<#
-.Synopsis
-Interact with Falcon Identity using GraphQL
-.Description
-Requires 'identity-graphql:write'.
-.Parameter Query
-GraphQL query statement
-.Example
-PS>Invoke-FalconIdentityGraph -Query 'entities(roles:[BuiltinAdministratorRole] sortKey:PRIMARY_DISPLAY_NAME
-sortOrder:ASCENDING first:5) {nodes{primaryDisplayName secondaryDisplayName}}'
-
-Query the primary and secondary display names for the first 5 Administrator accounts, sorted in ascending order
-by primary display name.
-.Example
-PS>Invoke-FalconIdentityGraph -Query 'entities(types:[USER] minRiskScoreSeverity:MEDIUM sortKey:
-RISK_SCORE sortOrder:DESCENDING first:10) {nodes{primaryDisplayName secondaryDisplayName isHuman:hasRole(
-type:HumanUserAccountRole) isProgrammatic:hasRole(type:ProgrammaticUserAccountRole) ... on UserEntity{
-emailAddresses} riskScore riskScoreSeverity riskFactors {type severity}}}'
-
-Query the top 10 users with the highest risk score and display basic information about their accounts and
-the risk factors contributing to their score.
-#>
     [CmdletBinding(DefaultParameterSetName = '/identity-protection/combined/graphql/v1:post')]
     param(
         [Parameter(ParameterSetName = '/identity-protection/combined/graphql/v1:post', Mandatory = $true,
