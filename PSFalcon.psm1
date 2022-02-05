f231017b0379ba4f1bf02e44c1c031c8d465df35
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-05 10:20:38 -0800

    v2.1.8
    Slightly changed formatting for .psm1 change

--- PSFalcon.psm1
+++ PSFalcon.psm1
@@ -1,15 +1,6 @@
-# Import public and private functions
-$imports = @(
+@(
+    # Import class, public and private functions
     Get-ChildItem -Path $PSScriptRoot\Public\*.ps1
     Get-ChildItem -Path $PSScriptRoot\Private\Private.ps1
     Get-ChildItem -Path $PSScriptRoot\Class\Class.ps1
-)
-
-$imports.foreach{
-    try {
-        . $_.FullName
-    }
-    catch {
-        throw $_
-    }
-}
+).foreach{ try { . $_.FullName } catch { throw $_ }}
