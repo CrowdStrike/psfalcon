aa25f4d1fe7287cef0ed6be5b96dca8e43ed6149
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-04-18 10:27:38 -0700

    Update psf-sensors.ps1

--- Public/psf-sensors.ps1
+++ Public/psf-sensors.ps1
@@ -294,7 +294,7 @@ https://github.com/crowdstrike/psfalcon/wiki/Host-and-Host-Group-Management
             Linux = 'manager=("$(if [[ $(command -v apt) ]]; then echo "apt-get purge falcon-sensor -y"; elif ' +
                 '[[ $(command -v yum) ]]; then echo "yum remove falcon-sensor -y"; elif [[ $(command -v zypper) ' +
                 ']]; then echo "zypper remove -y falcon-sensor"; fi)"); if [[ ${manager} ]]; then echo "Beginnin' +
-                'g removal of the Falcon sensor" && eval "sudo ${manager} &" &>/dev/null; else echo "apt,yum or' +
+                'g removal of the Falcon sensor" && eval "sudo ${manager} &" &>/dev/null; else echo "apt, yum or' +
                 ' zypper must be present to begin removal"; fi'
             Mac = $null
             Windows = 'Start-Sleep -Seconds 5; $RegPath = if ((Get-WmiObject win32_operatingsystem).osarchitectu' +
