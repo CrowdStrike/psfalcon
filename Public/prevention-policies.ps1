6a300e3b4c1de9485731131713209d08bcd45e9f
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-04-28 10:21:11 -0700

    Update prevention-policies.ps1

--- Public/prevention-policies.ps1
+++ Public/prevention-policies.ps1
@@ -172,7 +172,7 @@ https://github.com/crowdstrike/psfalcon/wiki/Detection-and-Prevention-Policies
             $Request = Invoke-Falcon @Param -Inputs $PSBoundParameters
         }
         if ($Request -and $Include) {
-            if (!$Request.id) { [object[]]$Request = @($Request).foreach{ [PSCustomObject]@{ id = $_ }}}
+            if (!$Request.id) { $Request = @($Request).foreach{ ,[PSCustomObject]@{ id = $_ }}}
             if ($Include -contains 'members') {
                 foreach ($i in $Request) {
                     $SetParam = @{
