e48290820c69d56c9578f8f6dd3f53f40cd54a17
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-04-28 10:21:09 -0700

    Update host-group.ps1

--- Public/host-group.ps1
+++ Public/host-group.ps1
@@ -126,7 +126,7 @@ https://github.com/crowdstrike/psfalcon/wiki/Host-and-Host-Group-Management
             $Request = Invoke-Falcon @Param -Inputs $PSBoundParameters
         }
         if ($Request -and $Include) {
-            if (!$Request.id) { [object[]]$Request = @($Request).foreach{ [PSCustomObject]@{ id = $_ }}}
+            if (!$Request.id) { $Request = @($Request).foreach{ ,[PSCustomObject]@{ id = $_ }}}
             if ($Include -contains 'members') {
                 foreach ($i in $Request) {
                     $SetParam = @{
