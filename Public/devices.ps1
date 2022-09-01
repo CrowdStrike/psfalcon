26b74029433ee2729d4bcb204e550ecf0ad890b2
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-09-01 14:56:48 -0700

    Bugfix for '-Include group_names'
    Fixed issue where group information wasn't being properly appended.

--- Public/devices.ps1
+++ Public/devices.ps1
@@ -224,12 +224,10 @@ https://github.com/crowdstrike/psfalcon/wiki/Host-and-Host-Group-Management
                     }
                 }
                 if ($Include -contains 'group_names') {
-                    $Groups = try {
-                        Get-FalconHostGroup -Id $Request.groups -EA 0 | Select-Object id,name
-                    } catch {}
+                    $Groups = try { $Request.groups | Get-FalconHostGroup -EA 0 | Select-Object id,name } catch {}
                     if ($Groups) {
                         foreach ($i in $Request) {
-                            $i.groups = @($Groups | Where-Object { $i.groups -contains $_.id })
+                            $i.groups = $Groups | Where-Object { $i.groups -contains $_.id }
                         }
                     }
                 }
