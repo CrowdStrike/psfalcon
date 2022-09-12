970dbd5c71b62c09cb6a35b7b9dfb3b3272ffeae
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-09-12 09:57:13 -0700

    Update identity-protection.ps1

--- Public/identity-protection.ps1
+++ Public/identity-protection.ps1
@@ -1,15 +1,14 @@
 function Invoke-FalconIdentityGraph {
-
-  <#
-  .SYNOPSIS
-  Interact with Falcon Identity using GraphQL
-  .DESCRIPTION
-  Requires 'Identity Protection GraphQL: Write'.
-  .PARAMETER Query
-  A complete GraphQL query statement
-  .PARAMETER All
-  Repeat requests until all available results are retrieved
-  #>
+<#
+.SYNOPSIS
+Interact with Falcon Identity using GraphQL
+.DESCRIPTION
+Requires 'Identity Protection GraphQL: Write'.
+.PARAMETER Query
+A complete GraphQL query statement
+.PARAMETER All
+Repeat requests until all available results are retrieved
+#>
     [CmdletBinding(DefaultParameterSetName='/identity-protection/combined/graphql/v1:post',SupportsShouldProcess)]
     param(
         [Parameter(ParameterSetName='/identity-protection/combined/graphql/v1:post',Mandatory,ValueFromPipeline,
@@ -104,5 +103,4 @@ function Invoke-FalconIdentityGraph {
         }
     }
     end { if ($PSBoundParameters.All -and $Request) { Invoke-GraphLoop $Request $Param $PSBoundParameters }}
-  }
-    
+}
