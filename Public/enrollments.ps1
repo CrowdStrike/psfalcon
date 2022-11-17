650eeec70028827064df59e376cfa5ac098c3db7
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-11-16 16:02:43 -0800

    Create enrollments.ps1

---
+++ Public/enrollments.ps1
@@ -0,0 +1,50 @@
+function Invoke-FalconMobileAction {
+<#
+.SYNOPSIS
+Trigger on-boarding process for a mobile device
+.DESCRIPTION
+Requires 'Mobile Enrollment: Write'.
+.PARAMETER Name
+Action to perform
+.PARAMETER ExpiresAt
+Expiration time [default: 30 days]
+.PARAMETER Email
+Email address
+.LINK
+https://github.com/crowdstrike/psfalcon/wiki/Invoke-FalconMobileAction
+#>
+       [CmdletBinding(DefaultParameterSetName='/enrollments/entities/details/v3:post',SupportsShouldProcess)]
+       param(
+            [Parameter(ParameterSetName='/enrollments/entities/details/v3:post',Mandatory,Position=1)]
+            [ValidateSet('enroll','re-enroll',IgnoreCase=$false)]
+            [Alias('action_name')]
+            [string]$Name,
+            [Parameter(ParameterSetName='/enrollments/entities/details/v3:post',Position=2)]
+            [Alias('expires_at')]
+            [string]$ExpiresAt,
+            [Parameter(ParameterSetName='/enrollments/entities/details/v3:post',Mandatory,ValueFromPipeline,
+                ValueFromPipelineByPropertyName,Position=3)]
+            [ValidateScript({
+                if ((Test-RegexValue $_) -eq 'email') { $true } else { throw "'$_' is not a valid email address." }
+            })]
+            [Alias('email_addresses')]
+            [string[]]$Email
+       )
+       # 
+       begin {
+        $Param = @{
+            Command = $MyInvocation.MyCommand.Name
+            Endpoint = $PSCmdlet.ParameterSetName
+            Format = @{ Query = @('action_name'); Body = @{ root = @('email_addresses','expires_at') }}
+        }
+        [System.Collections.Generic.List[string]]$List = @()
+        if (!$PSBoundParameters.ExpiresAt) { $PSBoundParameters.ExpiresAt = Convert-Rfc3339 720 }
+    }
+    process { if ($Email) { @($Email).foreach{ $List.Add($_) }}}
+    end {
+        if ($List) {
+            $PSBoundParameters['Email'] = @($List | Select-Object -Unique)
+            Invoke-Falcon @Param -Inputs $PSBoundParameters
+        }
+    }
+}
