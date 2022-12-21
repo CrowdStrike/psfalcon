b288ec8ebc26d01fc6de8c767490ba840cedf02b
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-12-21 14:30:34 -0800

    Update psf-fwmgr.ps1

--- Public/psf-fwmgr.ps1
+++ Public/psf-fwmgr.ps1
@@ -1,9 +1,15 @@
 function ConvertTo-FalconFirewallRule {
 <#
 .SYNOPSIS
-
+Convert firewall rules to be compatible with Falcon Firewall Management
 .DESCRIPTION
+Ensures that an object (either from the pipeline, or via CSV import) has the required properties to be accepted
+as a valid Falcon Firewall Management rule.
 
+Rules that contain both IPv4 and IPv6 addresses will generate errors, along with any rules that are missing the
+required properties defined by the 'Map' parameter.
+
+Converted rules used with 'New-FalconFirewallGroup' to create groups containing newly converted rules.
 .PARAMETER Map
 A hashtable containing the following keys with the corresponding CSV column or rule property as the value
 
