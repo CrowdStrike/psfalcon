4b362886ba87aed1a59b8b07a1732732db5eea24
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-02-02 15:42:10 -0800

    Update PSFalcon.psm1
    Added import of `PSFalcon.ps1xml` for object formatting

--- PSFalcon.psm1
+++ PSFalcon.psm1
@@ -3,4 +3,8 @@
     Get-ChildItem -Path $PSScriptRoot\Public\*.ps1
     Get-ChildItem -Path $PSScriptRoot\Private\Private.ps1
     Get-ChildItem -Path $PSScriptRoot\Class\Class.ps1
-).foreach{ try { . $_.FullName } catch { throw $_ }}
+).foreach{
+    try { . $_.FullName } catch { throw $_ }
+}
+# Import object formats for successful API responses
+$Script:Response = try { Get-Content $PSScriptRoot\Response\Response.json -EA 0 | ConvertFrom-Json } catch {}
