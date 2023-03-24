0d1773c69a94d6b53230e4cb737d66e9057fa6f5
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-03-24 14:51:05 -0700

    Update create-a-host-group-and-add-a-list-of-devices-by-hostname.ps1

--- Samples/host_groups/create-a-host-group-and-add-a-list-of-devices-by-hostname.ps1
+++ Samples/host_groups/create-a-host-group-and-add-a-list-of-devices-by-hostname.ps1
@@ -1,5 +1,15 @@
 #Requires -Version 5.1
 using module @{ModuleName='PSFalcon';ModuleVersion ='2.2'}
+<#
+.SYNOPSIS
+Create a host group, ingest a list of hostnames from a text file and add the corresponding hosts to the group
+.PARAMETER Path
+Path to a list of hostnames (one per line)
+.PARAMETER Name
+Name of host group to create
+.PARAMETER Description
+Description of the host group to create
+#>
 param(
     [Parameter(Mandatory,Position=1)]
     [ValidatePattern('\.txt$')]
