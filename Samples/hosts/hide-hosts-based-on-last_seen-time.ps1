0af2a0fd17267a52fc5ea21e0aa4ed6bffad5ebd
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-03-27 12:45:53 -0700

    Update hide-hosts-based-on-last_seen-time.ps1

--- Samples/hosts/hide-hosts-based-on-last_seen-time.ps1
+++ Samples/hosts/hide-hosts-based-on-last_seen-time.ps1
@@ -1,5 +1,11 @@
 #Requires -Version 5.1
 using module @{ModuleName='PSFalcon';ModuleVersion ='2.2'}
+<#
+.SYNOPSIS
+Hide hosts that have not been seen within a certain number of days
+.PARAMETER Days
+Hide hosts last seen more than this many days ago
+#>
 param(
     [Parameter(Mandatory)]
     [ValidateRange(1,44)]
