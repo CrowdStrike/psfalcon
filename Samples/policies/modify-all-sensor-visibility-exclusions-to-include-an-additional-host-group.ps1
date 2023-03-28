908385f790512d5ddd48832735ba978f26d997e5
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-03-28 14:52:29 -0700

    Update modify-all-sensor-visibility-exclusions-to-include-an-additional-host-group.ps1

--- Samples/policies/modify-all-sensor-visibility-exclusions-to-include-an-additional-host-group.ps1
+++ Samples/policies/modify-all-sensor-visibility-exclusions-to-include-an-additional-host-group.ps1
@@ -1,5 +1,11 @@
 #Requires -Version 5.1
 using module @{ModuleName='PSFalcon';ModuleVersion='2.2'}
+<#
+.SYNOPSIS
+Add a host group to all Sensor Visibility Exclusions
+.PARAMETER GroupId
+Group identifier
+#>
 param(
     [Parameter(Mandatory,Position=1)]
     [ValidatePattern('^[a-fA-F0-9]{32}$')]
