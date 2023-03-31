0420b5be1806f54446e69b085d00b69fce520c0c
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-03-31 12:16:18 -0700

    Update download-your-most-recent-scheduled-report-results.ps1

--- samples/scheduled_reports/download-your-most-recent-scheduled-report-results.ps1
+++ samples/scheduled_reports/download-your-most-recent-scheduled-report-results.ps1
@@ -2,10 +2,11 @@
 using module @{ModuleName='PSFalcon';ModuleVersion ='2.2'}
 <#
 .SYNOPSIS
-
-.PARAMETER
-
+Download your most recent successful Scheduled Report (or Search) execution results
 #>
-(Get-FalconScheduledReport -Detailed -All).last_execution | ForEach-Object {
-    Receive-FalconScheduledReport -Id $_.id -Path "$($_.result_metadata.report_file_name)"
+Get-FalconScheduledReport -Detailed -All | Where-Object { $_.last_execution } |
+Select-Object -ExpandProperty last_execution | Where-Object { $_.status -eq 'DONE' -and $_.result_metadata } |
+ForEach-Object {
+    Receive-FalconScheduledReport -Id $_.id -Path (
+        Join-Path (Get-Location).Path $_.result_metadata.report_file_name)
 }
