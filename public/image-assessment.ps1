2f02f2aa028a3c12e9023211d2bdd2c5334ed250
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-04-28 08:54:08 -0700

    Update image-assessment.ps1
    Added `Application` parameter for filtering application packages

--- public/image-assessment.ps1
+++ public/image-assessment.ps1
@@ -9,6 +9,8 @@ Operating system version
 .PARAMETER Package
 Key and value pairs to filter packages. Accepted properties include: 'layerindex', 'packageprovider', 'layerhash',
 'packagehash', 'packagesource', 'softwarearchitecture', 'status', 'majorversion', 'product', and 'vendor'.
+.PARAMETER Application
+Key and value pairs to filter application packages. Accepted properties include: 'libraries' and 'type'.
 .LINK
 https://github.com/crowdstrike/psfalcon/wiki/Get-FalconContainerVulnerability
 #>
@@ -31,7 +33,21 @@ https://github.com/crowdstrike/psfalcon/wiki/Get-FalconContainerVulnerability
             }
         })]
         [Alias('packages')]
-        [object[]]$Package
+        [object[]]$Package,
+        [Parameter(ParameterSetName='/image-assessment/combined/vulnerability-lookups/v1:post',Position=3)]
+        [ValidateScript({
+            foreach ($Object in $_) {
+                $Param = @{
+                    Object = $Object
+                    Command = 'Get-FalconContainerVulnerability'
+                    Endpoint = '/image-assessment/combined/vulnerability-lookups/v1:post'
+                    Allowed = @('libraries','type')
+                }
+                Confirm-Parameter @Param
+            }
+        })]
+        [Alias('applicationPackages')]
+        [object[]]$Application
     )
     begin {
         $Param = @{
