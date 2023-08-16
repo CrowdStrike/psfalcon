6349414f912e50b83abd85560d7843a7f8c7efd9
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-08-16 14:44:52 -0700

    Update remove_sensortag.sh

--- script/remove_sensortag.sh
+++ script/remove_sensortag.sh
@@ -1,15 +1,8 @@
-IFS=,
-read -ra del <<< "$1"
-read -ra tag <<< "$(/opt/CrowdStrike/falconctl -g --tags | sed "s/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//")"
-if [[ ${tag[@]} ]]
-then
+IFS=, && read -ra del <<< "$1" && read -ra tag <<< "$(/opt/CrowdStrike/falconctl -g --tags | sed "s/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//")"
+if [[ ${tag[@]} ]]; then
   /opt/CrowdStrike/falconctl -d -f --tags
-  for i in ${del[@]}
-    do tag=(${tag[@]/$i})
-  done
-  IFS=$'\n'
-  val=($(printf "%s\n" ${tag[*]} | xargs))
-  val="$(echo ${val[*]} | tr " " ",")"
+  for i in ${del[@]}; do tag=(${tag[@]/$i}); done
+  IFS=$'\n' && val=($(printf "%s\n" ${tag[*]} | xargs)) && val="$(echo ${val[*]} | tr " " ",")"
   /opt/CrowdStrike/falconctl -s --tags="$val"
 fi
 /opt/CrowdStrike/falconctl -g --tags | sed "s/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//"
