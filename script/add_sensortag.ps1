571252aabc8a7615b9c7276c6c68a36b869a63f5
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-08-16 12:03:13 -0700

    Create add_sensortag.ps1

---
+++ script/add_sensortag.ps1
@@ -0,0 +1,11 @@
+param([string]$Token,[string]$Tag)
+$E=Join-Path $env:ProgramFiles "CrowdStrike\CsSensorSettings.exe";
+if (Test-Path $E) {
+  if ($Token) {
+    echo "$Token" | & "$E" set --grouping-tags "$Tag"
+  } else {
+    & "$E" set --grouping-tags "$Tag"
+  }
+} else {
+  throw "Not found: $E"
+}
