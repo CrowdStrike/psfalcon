32178a2c33d3c3ebe5a72232b680027864a5b5d0
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-08-16 14:17:40 -0700

    Update uninstall_sensor.ps1

--- script/uninstall_sensor.ps1
+++ script/uninstall_sensor.ps1
@@ -1,8 +1,8 @@
-$t=$args;
-$sp=Join-Path $env:ProgramFiles (Join-Path "CrowdStrike" "CSFalconService.exe");
+$t=$args
+$sp=Join-Path $env:ProgramFiles (Join-Path "CrowdStrike" "CSFalconService.exe")
 if (!(Test-Path $sp)) { throw "Unable to locate $sp" }
 $bv=if((Get-CimInstance win32_operatingsystem).OSArchitecture -match "64"){ "WOW6432Node\" }
-$rp="HKLM:\SOFTWARE\$($bv)Microsoft\Windows\CurrentVersion\Uninstall";
+$rp="HKLM:\SOFTWARE\$($bv)Microsoft\Windows\CurrentVersion\Uninstall"
 if (!(Test-Path $rp)) { throw "Unable to locate $rp" }
 @(gci $rp).Where({ $_.GetValue("DisplayName") -match "CrowdStrike(.+)?Sensor" }).foreach{
   if ((gi $sp).VersionInfo.FileVersion -eq $_.GetValue("DisplayVersion")) {
@@ -12,9 +12,9 @@ if (!(Test-Path $rp)) { throw "Unable to locate $rp" }
       $_.GetValue("UninstallString")
     }
     if (!$us) { throw "Failed to find UninstallString value for $($_.GetValue("DisplayName"))" }
-    $al=@("/c",$us);
+    $al=@("/c",$us)
     if ($t) { $al+="MAINTENANCE_TOKEN=$t" }
-    "Starting removal of the Falcon sensor";
+    "Starting removal of the Falcon sensor"
     [void](start -FilePath cmd.exe -ArgumentList ($al -join " ") -PassThru)
   }
 }
