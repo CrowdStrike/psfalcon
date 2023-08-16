b4cb76627d49a56b11951d8040f017b54cfa4856
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-08-16 12:03:25 -0700

    Create uninstall_sensor.ps1

---
+++ script/uninstall_sensor.ps1
@@ -0,0 +1,20 @@
+$t=$args;
+$sp=Join-Path $env:ProgramFiles (Join-Path "CrowdStrike" "CSFalconService.exe");
+if (!(Test-Path $sp)) { throw "Unable to locate $sp" }
+$bv=if((Get-CimInstance win32_operatingsystem).OSArchitecture -match "64"){ "WOW6432Node\" }
+$rp="HKLM:\SOFTWARE\$($bv)Microsoft\Windows\CurrentVersion\Uninstall";
+if (!(Test-Path $rp)) { throw "Unable to locate $rp" }
+@(gci $rp).Where({ $_.GetValue("DisplayName") -match "CrowdStrike(.+)?Sensor" }).foreach{
+  if ((gi $sp).VersionInfo.FileVersion -eq $_.GetValue("DisplayVersion")) {
+    $us=if ($_.GetValue("QuietUninstallString")) {
+      $_.GetValue("QuietUninstallString")
+    } else {
+      $_.GetValue("UninstallString")
+    }
+    if (!$us) { throw "Failed to find UninstallString value for $($_.GetValue("DisplayName"))" }
+    $al=@("/c",$us);
+    if ($t) { $al+="MAINTENANCE_TOKEN=$t" }
+    "Starting removal of the Falcon sensor";
+    [void](start -FilePath cmd.exe -ArgumentList ($al -join " ") -PassThru)
+  }
+}
