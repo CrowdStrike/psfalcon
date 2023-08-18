21b31479092296cd2a31fd5b3c07351492428bb0
bk-cs <54042976+bk-cs@users.noreply.github.com>
2023-08-18 11:39:51 -0700

    Update uninstall_sensor.ps1
    Lengthened variable names and removed aliases

--- script/uninstall_sensor.ps1
+++ script/uninstall_sensor.ps1
@@ -1,20 +1,20 @@
-$t=$args
-$sp=Join-Path $env:ProgramFiles (Join-Path "CrowdStrike" "CSFalconService.exe")
-if (!(Test-Path $sp)) { throw "Unable to locate $sp" }
-$bv=if((Get-CimInstance win32_operatingsystem).OSArchitecture -match "64"){ "WOW6432Node\" }
-$rp="HKLM:\SOFTWARE\$($bv)Microsoft\Windows\CurrentVersion\Uninstall"
-if (!(Test-Path $rp)) { throw "Unable to locate $rp" }
-@(gci $rp).Where({ $_.GetValue("DisplayName") -match "CrowdStrike(.+)?Sensor" }).foreach{
-  if ((gi $sp).VersionInfo.FileVersion -eq $_.GetValue("DisplayVersion")) {
-    $us=if ($_.GetValue("QuietUninstallString")) {
+$Token = $args
+$ServicePath = Join-Path $env:ProgramFiles (Join-Path "CrowdStrike" "CSFalconService.exe")
+if (!(Test-Path $ServicePath)) { throw "Unable to locate $ServicePath" }
+$BitValue = if((Get-CimInstance win32_operatingsystem).OSArchitecture -match "64"){ "WOW6432Node\" }
+$RegPath = "HKLM:\SOFTWARE\$($BitValue)Microsoft\Windows\CurrentVersion\Uninstall"
+if (!(Test-Path $RegPath)) { throw "Unable to locate $RegPath" }
+@(Get-ChildItem $RegPath).Where({ $_.GetValue("DisplayName") -match "CrowdStrike(.+)?Sensor" }).foreach{
+  if ((Get-Item $ServicePath).VersionInfo.FileVersion -eq $_.GetValue("DisplayVersion")) {
+    $UninstallString = if ($_.GetValue("QuietUninstallString")) {
       $_.GetValue("QuietUninstallString")
     } else {
       $_.GetValue("UninstallString")
     }
-    if (!$us) { throw "Failed to find UninstallString value for $($_.GetValue("DisplayName"))" }
-    $al=@("/c",$us)
-    if ($t) { $al+="MAINTENANCE_TOKEN=$t" }
+    if (!$UninstallString) { throw "Failed to find UninstallString value for $($_.GetValue("DisplayName"))" }
+    $ArgList = @("/c",$UninstallString)
+    if ($Token) { $ArgList += "MAINTENANCE_TOKEN=$Token" }
     "Starting removal of the Falcon sensor"
-    [void](start -FilePath cmd.exe -ArgumentList ($al -join " ") -PassThru)
+    [void](Start-Process -FilePath cmd.exe -ArgumentList ($ArgList -join " ") -PassThru)
   }
 }
