c68016ac76c578eef8f83cf4cfc117f3ee3ef95e
bk-CS <54042976+bk-cs@users.noreply.github.com>
2023-10-06 10:38:46 -0700

    Updated remove_sensortag.sh to resolve bug where existing tags would be modified to remove matching tag values (i.e. "another" becoming "an" when attempting to remove "other").  # Please enter the commit message for your changes. Lines starting

--- script/remove_sensortag.sh
+++ script/remove_sensortag.sh
@@ -1,8 +1,17 @@
 IFS=, && read -ra del <<< "$1" && read -ra tag <<< "$(/opt/CrowdStrike/falconctl -g --tags | sed "s/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//")"
 if [[ ${tag[@]} ]]; then
   /opt/CrowdStrike/falconctl -d -f --tags
-  for i in ${del[@]}; do tag=(${tag[@]/$i}); done
-  IFS=$'\n' && val=($(printf "%s\n" ${tag[*]} | xargs)) && val="$(echo ${val[*]} | tr " " ",")"
+  for todelete in ${del[@]}
+  do
+    for i in "${!tag[@]}"
+    do
+      if [ ${tag[i]} = $todelete ]
+      then
+        unset 'tag[i]'
+      fi
+    done
+  done
+  val="$(echo ${tag[@]} | tr ' ' ',')"
   /opt/CrowdStrike/falconctl -s --tags="$val"
 fi
 /opt/CrowdStrike/falconctl -g --tags | sed "s/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//"
