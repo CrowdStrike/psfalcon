2de0b88dbaf5be2c2e27be097b49e2d15d7b673b
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-03-06 13:26:04 -0800

    Update retrieve_hosts_and_their_applications.ps1
    Updated sample script to output to Json and added some host stream output to show progress

--- samples/discover/retrieve_hosts_and_their_applications.ps1
+++ samples/discover/retrieve_hosts_and_their_applications.ps1
@@ -2,7 +2,7 @@
 using module @{ModuleName='PSFalcon';ModuleVersion='2.2'}
 <#
 .SYNOPSIS
-Retrieve all hosts with Falcon installed and append their installed applications to final output
+Retrieve all hosts with Falcon installed, append their installed applications, then output to Json
 .DESCRIPTION
 Requires "Hosts: Read" and "Falcon Discover: Read"
 .PARAMETER ClientId
@@ -28,33 +28,53 @@ param(
   [string]$Hostname
 )
 begin {
+  function Add-AppList ([object[]]$HostObj,[object[]]$AppObj) {
+    foreach ($Member in $HostObj) {
+      # Append installed applications to final host output
+      $Member.PSObject.Properties.Add((New-Object PSNoteProperty('applications',(@($AppObj).Where({
+        $_.host.aid -eq $Member.device_id })))))
+      Write-Host "Matched $(($Member.applications.id | Measure-Object).Count) applications to host $(
+        $Member.device_id)."
+      $Member
+    }
+  }
   # Request authorization token
   $Token = @{}
   @('ClientId','ClientSecret','Hostname').foreach{
     if ($PSBoundParameters.$_) { $Token[$_] = $PSBoundParameters.$_ }
   }
   Request-FalconToken @Token
+  [string]$Json = "hosts_and_applications_$(Get-Date -Format FileDateTime).json"
 }
 process {
   # Retrieve all hosts and their details
+  Write-Host "Start $(Get-Date -Format o)"
   $HostList = Get-FalconHost -Detailed -All
-  for ($i=0; $i -lt ($HostList.device_id | Measure-Object).Count; $i+=100) {
-    # In groups of 100 ids, retrieve list of installed applications
-    [string[]]$IdGroup = @($HostList.device_id)[$i..($i+99)]
-    $Param = @{
-      Filter = "host.aid:[$((@($IdGroup).foreach{ "'$_'"}) -join ',')]"
-      Application = $true
-      Detailed = $true
-      All = $true
-    }
-    $AppList = Get-FalconAsset @Param
-    foreach ($Id in $IdGroup) {
-      @($HostList).Where({ $_.device_id -eq $Id }).foreach{
-        # Append installed applications to host output
-        $_.PSObject.Properties.Add((New-Object PSNoteProperty('applications',(
-          @($AppList).Where({ $_.host.aid -eq $Id })))))
+  $HostTotal = ($HostList.device_id | Measure-Object).Count
+  Write-Host "Retrieved $HostTotal host results."
+  $Output = for ($i=0; $i -lt $HostTotal; $i+=100) {
+    # Check total count of applications using group of 100 ids
+    [object[]]$Group = @($HostList)[$i..($i+99)]
+    [string]$Filter = "host.aid:[$((@($Group).foreach{ "'$($_.device_id)'" }) -join ',')]"
+    [int32]$AppTotal = Get-FalconAsset -Filter $Filter -Application -Total
+    if ($AppTotal -lt 10000) {
+      # Request all 100 hosts if total application results are less than 10,000
+      Write-Host "Requesting $AppTotal applications for hosts $($Group[0].device_id) to $(
+        $Group[-1].device_id) [$i of $HostTotal]"
+      Add-AppList $Group (Get-FalconAsset -Filter $Filter -Application -Detailed -All)
+    } else {
+      for ($i2=0; $i2 -lt ($Group.device_id | Measure-Object).Count; $i2+=20) {
+        # Break group of 100 into groups of 20 to stay under 10,000 results
+        [object[]]$SubGroup = @($Group)[$i2..($i2+19)]
+        [string]$SubFilter = "host.aid:[$((@($SubGroup).foreach{ "'$($_.device_id)'" }) -join ',')]"
+        [int32]$SubTotal = Get-FalconAsset -Filter $SubFilter -Application -Total
+        Write-Host "Requesting $SubTotal applications for hosts $($SubGroup[0].device_id) to $(
+          $SubGroup[-1].device_id) [$($i + $i2) of $HostTotal]"
+        Add-AppList $SubGroup (Get-FalconAsset -Filter $SubFilter -Application -Detailed -All)
       }
     }
   }
-  $HostList
+  $Json | ConvertTo-Json -Depth 32 > $Output
+  Write-Host "End $(Get-Date -Format o)"
 }
+end { if (Test-Path $Json) { Get-ChildItem $Json | Select-Object FullName,Length,LastWriteTime }}
