5be2111918f48a2b0857e549945c753688b19caf
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-05-20 16:11:54 -0700

    Update loggingapi.ps1

--- public/loggingapi.ps1
+++ public/loggingapi.ps1
@@ -18,6 +18,54 @@ https://github.com/crowdstrike/psfalcon/wiki/Get-FalconFoundryRepository
   begin { $Param = @{ Command = $MyInvocation.MyCommand.Name; Endpoint = $PSCmdlet.ParameterSetName }}
   process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
 }
+function Get-FalconFoundrySearch {
+<#
+.SYNOPSIS
+Get the results of a saved search
+.DESCRIPTION
+Requires 'App Logs: Read'.
+.PARAMETER Id
+Search identifier
+.PARAMETER AppId
+Foundry application identifier
+.PARAMETER InferJsonTypes
+Whether to try to infer data types in json event response instead of returning map[string]string
+.PARAMETER Limit
+Maximum number of results per request
+.PARAMETER MatchResponseSchema
+Whether to validate search results against their schema
+.PARAMETER Metadata
+Whether to include metadata in the response
+.PARAMETER Offset
+Position to begin retrieving results
+.LINK
+https://github.com/crowdstrike/psfalcon/wiki/Get-FalconFoundrySearch
+#>
+  [CmdletBinding(DefaultParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',
+    SupportsShouldProcess)]
+  param(
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Mandatory,Position=0)]
+    [Alias('job_id')]
+    [string]$Id,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
+    [Alias('app_id')]
+    [string]$AppId,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
+    [Alias('infer_json_types')]
+    [boolean]$InferJsonTypes,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
+    [string]$Limit,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
+    [Alias('match_response_schema')]
+    [boolean]$MatchResponseSchema,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
+    [boolean]$Metadata,
+    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get')]
+    [string]$Offset
+  )
+  begin { $Param = @{ Command = $MyInvocation.MyCommand.Name; Endpoint = $PSCmdlet.ParameterSetName }}
+  process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
+}
 function Get-FalconFoundryView {
 <#
 .SYNOPSIS
@@ -178,53 +226,6 @@ https://github.com/crowdstrike/psfalcon/wiki/New-CommandName
     [object]$SearchQueryArgs,
     [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute-dynamic/v1:post',Position=0)]
     [string]$Start
-  )
-  begin { $Param = @{ Command = $MyInvocation.MyCommand.Name; Endpoint = $PSCmdlet.ParameterSetName }}
-  process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
-}
-function Get-FalconFoundrySearch {
-<#
-.SYNOPSIS
-Get the results of a saved search
-.DESCRIPTION
-Requires 'App Logs: Read'.
-.PARAMETER Id
-Search identifier
-.PARAMETER AppId
-Foundry application identifier
-.PARAMETER InferJsonTypes
-Whether to try to infer data types in json event response instead of returning map[string]string
-.PARAMETER Limit
-Maximum number of results per request
-.PARAMETER MatchResponseSchema
-Whether to validate search results against their schema
-.PARAMETER Metadata
-Whether to include metadata in the response
-.PARAMETER Offset
-Position to begin retrieving results
-.LINK
-https://github.com/crowdstrike/psfalcon/wiki/Get-FalconFoundrySearch
-#>
-  [CmdletBinding(DefaultParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',SupportsShouldProcess)]
-  param(
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Mandatory,Position=0)]
-    [Alias('job_id')]
-    [string]$Id,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
-    [Alias('app_id')]
-    [string]$AppId,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
-    [Alias('infer_json_types')]
-    [boolean]$InferJsonTypes,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
-    [string]$Limit,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
-    [Alias('match_response_schema')]
-    [boolean]$MatchResponseSchema,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get',Position=0)]
-    [boolean]$Metadata,
-    [Parameter(ParameterSetName='/loggingapi/entities/saved-searches/execute/v1:get')]
-    [string]$Offset
   )
   begin { $Param = @{ Command = $MyInvocation.MyCommand.Name; Endpoint = $PSCmdlet.ParameterSetName }}
   process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
