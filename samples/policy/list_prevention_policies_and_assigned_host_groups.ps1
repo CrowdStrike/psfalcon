f23a3c6d6496a764a5df7190a84627d93a37c451
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-06-05 10:18:27 -0700

    Update list_prevention_policies_and_assigned_host_groups.ps1

--- samples/policy/list_prevention_policies_and_assigned_host_groups.ps1
+++ samples/policy/list_prevention_policies_and_assigned_host_groups.ps1
@@ -10,8 +10,6 @@ A CSV will be created in the local directory containing the script results
 API client identifier
 .PARAMETER ClientSecret
 API client secret
-.PARAMETER Cloud
-Cloud value, used with authorization token request [default: us-1]
 .PARAMETER Hostname
 Hostname value, used with authorization token request [default: https://api.crowdstrike.com]
 .PARAMETER MemberCid
@@ -19,32 +17,26 @@ A list of one or more child CIDs to run the script against [default: all active 
 .EXAMPLE
 .\list_prevention_policies_and_assigned_host_groups.ps1 -ClientId abc -ClientSecret def -Cloud us-1
 #>
-[CmdletBinding(DefaultParameterSetName='Cloud')]
+[CmdletBinding()]
 param(
-  [Parameter(ParameterSetName='Cloud',Mandatory,Position=1)]
-  [Parameter(ParameterSetName='Hostname',Mandatory,Position=1)]
+  [Parameter(Mandatory,Position=1)]
   [ValidatePattern('^[a-fA-F0-9]{32}$')]
   [string]$ClientId,
-  [Parameter(ParameterSetName='Cloud',Mandatory,Position=2)]
-  [Parameter(ParameterSetName='Hostname',Mandatory,Position=2)]
+  [Parameter(Mandatory,Position=2)]
   [ValidatePattern('^\w{40}$')]
   [string]$ClientSecret,
-  [Parameter(ParameterSetName='Cloud',Position=3)]
-  [ValidateSet('us-1','us-2','us-gov-1','eu-1',IgnoreCase=$false)]
-  [string]$Cloud,
-  [Parameter(ParameterSetName='Hostname',Position=3)]
+  [Parameter(Position=3)]
   [ValidateSet('https://api.crowdstrike.com','https://api.us-2.crowdstrike.com',
     'https://api.laggar.gcw.crowdstrike.com','https://api.eu-1.crowdstrike.com',IgnoreCase=$false)]
   [string]$Hostname,
-  [Parameter(ParameterSetName='Cloud',Position=4)]
-  [Parameter(ParameterSetName='Hostname',Position=4)]
+  [Parameter(Position=4)]
   [ValidatePattern('^[a-fA-F0-9]{32}$')]
   [string[]]$MemberCid
 )
 begin {
   # Build hashtable for authorization token request
   $Token = @{}
-  @('ClientId','ClientSecret','Hostname','Cloud').foreach{
+  @('ClientId','ClientSecret','Hostname').foreach{
     if ($PSBoundParameters.$_) { $Token[$_] = $PSBoundParameters.$_ }
   }
   if (!$MemberCid) {
