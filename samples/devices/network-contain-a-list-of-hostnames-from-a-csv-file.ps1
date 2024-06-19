dad6fe231abafbb0bf7dc289febdeccf501abba4
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-06-19 11:37:47 -0700

    Update network-contain-a-list-of-hostnames-from-a-csv-file.ps1

--- samples/devices/network-contain-a-list-of-hostnames-from-a-csv-file.ps1
+++ samples/devices/network-contain-a-list-of-hostnames-from-a-csv-file.ps1
@@ -2,29 +2,37 @@
 using module @{ModuleName='PSFalcon';ModuleVersion ='2.2'}
 <#
 .SYNOPSIS
-Import a list of devices from CSV and contain them
+Import a list of hostnames from CSV and network contain matching devices
 .PARAMETER Path
-Path to the CSV file containing a list of devices
+Path to the CSV file containing a list of hostnames
 .NOTES
 This example requires a CSV with a column labeled 'hostname'. It will create a new CSV with that includes
-'hostname', 'device_id' and 'contain_requested' status.
+'hostname', 'device_id' and 'contain_requested' status in the current directory.
 #>
 param(
   [Parameter(Mandatory)]
   [ValidatePattern('\.csv$')]
   [string]$Path
 )
+# Create output file CSV name and import values from CSV
 [string]$OutputFile = Join-Path (Get-Location).Path "contained_$(Get-Date -Format FileDateTime).csv"
 $Import = Import-Csv -Path $Path
 if (!$Import.hostname) { throw "No 'hostname' column found in '$Path'." }
 
-# Use Find-FalconDuplicate to find duplicate hosts and contain them
-$Import.hostname | Find-FalconHostname -OutVariable HostList |
-  Invoke-FalconHostAction -Name contain -OutVariable ContainList
+# Search for hosts and capture the most recently seen result that matches the provided hostname
+[System.Collections.Generic.List[object]]$HostList = @()
+@($Import.hostname).Where({![string]::IsNullOrEmpty($_)}).foreach{
+  @(Get-FalconHost -Filter "hostname:['$_']" -Limit 1 -Sort last_seen.desc -Detailed | Select-Object device_id,
+  hostname).foreach{
+    $HostList.Add($_)
+  }
+}
 if (!$HostList.hostname) {
   # Error if no matches were found
   throw "No hosts found."
 } else {
+  # Contain devices
+  $HostList.device_id | Invoke-FalconHostAction -Name contain -OutVariable ContainList
   @($HostList).foreach{
     # Add 'contain_requested' property and export to CSV
     $Status = if ($ContainList.id -contains $_.device_id) { $true } else { $false }
