16f3e66cda9c1bd28c7f84e30b1ec22fc0f5ecc3
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-08-15 12:29:01 -0700

    Update cloud-connect-cspm-aws.ps1

--- public/cloud-connect-cspm-aws.ps1
+++ public/cloud-connect-cspm-aws.ps1
@@ -40,11 +40,6 @@ https://github.com/crowdstrike/psfalcon/wiki/Edit-FalconCloudAwsAccount
     [string]$AccountId,
     [Parameter(ParameterSetName='/cloud-connect-cspm-aws/entities/account/v1:patch',
       ValueFromPipelineByPropertyName,Position=2)]
-    [ValidateSet('af-south-1','ap-east-1','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-south-1',
-      'ap-south-2','ap-southeast-1','ap-southeast-2','ap-southeast-3','ap-southeast-4','ca-central-1',
-      'cn-north-1','cn-northwest-1','eu-central-1','eu-central-2','eu-north-1','eu-south-1','eu-south-2',
-      'eu-west-1','eu-west-2','eu-west-3','il-central-1','me-central-1','me-south-1','sa-east-1','us-east-1',
-      'us-east-2','us-gov-east-1','us-gov-west-1','us-west-1','us-west-2',IgnoreCase=$false)]
     [Alias('cloudtrail_region')]
     [string]$CloudtrailRegion,
     [Parameter(ParameterSetName='/cloud-connect-cspm-aws/entities/account/v1:patch',
@@ -236,11 +231,6 @@ https://github.com/crowdstrike/psfalcon/wiki/New-FalconCloudAwsAccount
     [Alias('account_id')]
     [string]$AccountId,
     [Parameter(ParameterSetName='/cloud-connect-cspm-aws/entities/account/v1:post',Mandatory,Position=2)]
-    [ValidateSet('af-south-1','ap-east-1','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-south-1',
-      'ap-south-2','ap-southeast-1','ap-southeast-2','ap-southeast-3','ap-southeast-4','ca-central-1',
-      'cn-north-1','cn-northwest-1','eu-central-1','eu-central-2','eu-north-1','eu-south-1','eu-south-2',
-      'eu-west-1','eu-west-2','eu-west-3','il-central-1','me-central-1','me-south-1','sa-east-1','us-east-1',
-      'us-east-2','us-gov-east-1','us-gov-west-1','us-west-1','us-west-2',IgnoreCase=$false)]
     [Alias('cloudtrail_region')]
     [string]$CloudtrailRegion,
     [Parameter(ParameterSetName='/cloud-connect-cspm-aws/entities/account/v1:post',ValueFromPipelineByPropertyName,
