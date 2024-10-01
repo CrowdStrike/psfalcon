b603fe7a395982a39aa09222a5ca08c221f62c06
bk-cs <54042976+bk-cs@users.noreply.github.com>
2024-10-01 10:53:16 -0700

    Issue #427
    Added `tar` to valid `Command` list for `Invoke-FalconAdminCommand` and `Invoke-FalconResponderCommand`
    
    Corrected `Invoke-FalconAdminCommand` to properly include the `Command` value `update query`

--- public/real-time-response.ps1
+++ public/real-time-response.ps1
@@ -541,12 +541,12 @@ https://github.com/crowdstrike/psfalcon/wiki/Invoke-FalconAdminCommand
     [Parameter(ParameterSetName='/real-time-response/entities/admin-command/v1:post',Mandatory,Position=1)]
     [Parameter(ParameterSetName='/real-time-response/combined/batch-admin-command/v1:post',Mandatory,
       Position=1)]
-    [ValidateSet('cat','cd','clear','cp','csrutil','cswindiag','encrypt','env','eventlog backup',
-      'eventlog export','eventlog list','eventlog view','falconscript','filehash','get','getsid','help','history',
-      'ifconfig','ipconfig','kill','ls','map','memdump','mkdir','mount','mv','netstat','ps','put',
-      'put-and-run','reg delete','reg load','reg query','reg set','reg unload','restart','rm','run',
-      'runscript','shutdown','umount','unmap','update history','update install','update list',
-      'update install','users','xmemdump','zip',IgnoreCase=$false)]
+    [ValidateSet('cat','cd','clear','cp','csrutil','cswindiag','encrypt','env','eventlog backup','eventlog export',
+      'eventlog list','eventlog view','falconscript','filehash','get','getsid','help','history','ifconfig',
+      'ipconfig','kill','ls','map','memdump','mkdir','mount','mv','netstat','ps','put','put-and-run','reg delete',
+      'reg load','reg query','reg set','reg unload','restart','rm','run','runscript','shutdown','tar','umount',
+      'unmap','update history','update install','update list','update query','users','xmemdump','zip',
+      IgnoreCase=$false)]
     [Alias('base_command')]
     [string]$Command,
     [Parameter(ParameterSetName='/real-time-response/entities/admin-command/v1:post',Position=2)]
@@ -882,8 +882,8 @@ https://github.com/crowdstrike/psfalcon/wiki/Invoke-FalconResponderCommand
     [ValidateSet('cat','cd','clear','cp','csrutil','encrypt','env','eventlog backup','eventlog export',
       'eventlog list','eventlog view','filehash','get','getsid','help','history','ifconfig','ipconfig','kill','ls',
       'map','memdump','mkdir','mount','mv','netstat','ps','reg delete','reg load','reg query','reg set',
-      'reg unload','restart','rm','runscript','shutdown','umount','unmap','update history','update install',
-      'update list','update install','users','xmemdump','zip',IgnoreCase=$false)]
+      'reg unload','restart','rm','runscript','shutdown','tar','umount','unmap','update history','update install',
+      'update list','update query','users','xmemdump','zip',IgnoreCase=$false)]
     [Alias('base_command')]
     [string]$Command,
     [Parameter(ParameterSetName='/real-time-response/entities/active-responder-command/v1:post',Position=2)]
