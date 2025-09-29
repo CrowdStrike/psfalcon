2d7192d23c4038175452bd574b86bcbf22da60d7
bk-cs <54042976+bk-cs@users.noreply.github.com>
2025-09-29 10:00:26 -0700

    Version change

--- class/Class.ps1
+++ class/Class.ps1
@@ -8,7 +8,7 @@ class ApiClient {
     $this.Handler = [System.Net.Http.HttpClientHandler]::New()
     $this.Client = [System.Net.Http.HttpClient]::New($this.Handler)
     $this.Client.Timeout = [System.TimeSpan]::New(0,10,0)
-    $this.UserAgent = 'crowdstrike-psfalcon/2.3.0'
+    $this.UserAgent = 'crowdstrike-psfalcon/2.2.10'
   }
   [string] Path([string]$Path) {
     $Output = if (![IO.Path]::IsPathRooted($Path)) {
