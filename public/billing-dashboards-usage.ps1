0bb175e7609a84840ff2466ea40aadf2ae5d8847
bk-cs <54042976+bk-cs@users.noreply.github.com>
2025-09-30 12:39:23 -0700

    Added parameter
    ### Get-FalconHostAverage
    * Added `Hourly` to utilize new `GET /billing-dashboards-usage/aggregates/hourly-average/v1` endpoint.

--- public/billing-dashboards-usage.ps1
+++ public/billing-dashboards-usage.ps1
@@ -1,11 +1,13 @@
 function Get-FalconHostAverage {
 <#
 .SYNOPSIS
-List Falcon weekly average host count
+List weekly or hourly average Falcon host count for the previous 28 days
 .DESCRIPTION
 Requires 'Sensor Usage: Read'.
 .PARAMETER Filter
 Falcon Query Language expression to limit results
+.PARAMETER Hourly
+Return hourly averages instead of weekly
 .LINK
 https://github.com/crowdstrike/psfalcon/wiki/Get-FalconHostAverage
 #>
@@ -13,8 +15,12 @@ https://github.com/crowdstrike/psfalcon/wiki/Get-FalconHostAverage
     SupportsShouldProcess)]
   param(
     [Parameter(ParameterSetName='/billing-dashboards-usage/aggregates/weekly-average/v1:get',Position=1)]
+    [Parameter(ParameterSetName='/billing-dashboards-usage/aggregates/hourly-average/v1:get',Position=1)]
     [ValidateScript({Test-FqlStatement $_})]
-    [string]$Filter
+    [string]$Filter,
+    [Parameter(ParameterSetName='/billing-dashboards-usage/aggregates/hourly-average/v1:get',Mandatory)]
+    [switch]$Hourly
+
   )
   begin { $Param = @{ Command = $MyInvocation.MyCommand.Name; Endpoint = $PSCmdlet.ParameterSetName }}
   process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
