f4b4c866cd0db94088240d60b801d23e6b228bbe
bk-cs <54042976+bk-cs@users.noreply.github.com>
2025-10-09 11:38:52 -0700

    Revert "Update Private.ps1"
    This reverts commit 22e76a192b06ca18c65bb4a85cf039c1e1c71d75.

--- private/Private.ps1
+++ private/Private.ps1
@@ -759,7 +759,7 @@ function Invoke-Falcon {
         }
         try {
           Write-Log $Command $Endpoint
-          $Request = try { $Script:Falcon.Api.Invoke($_.Endpoint) } catch { Write-Error 'no_content_from_api' }
+          $Request = $Script:Falcon.Api.Invoke($_.Endpoint)
           if ($Request) {
             # Capture pagination for 'Total' and 'All'
             $Pagination = $Request.meta.pagination
