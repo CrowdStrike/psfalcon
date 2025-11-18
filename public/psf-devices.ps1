1ec6539792a742ba32c37f8f5b358c5b604c178c
bk-cs <54042976+bk-cs@users.noreply.github.com>
2025-11-18 13:58:23 -0800

    Issue #514
    ### Find-FalconDuplicate
    * Renamed `Hosts` to `InputObject`. Kept `Hosts` as an alias.
    * Added pipeline support for `InputObject`.
    * Changed `Platform` to `[string[]]` so users can check multiple operating systems at the same time.
    * Added verbose message indicating when grouping by hostname starts.
    * Removed `first_seen` as a required property for final result output.
    * Increased performance using filtering of `InputObject` that only selects objects that contain required
      properties (`cid`,`hostname`,`last_seen`) instead of checking each object for required properties by name.
    * Added verbose message to indicate when a host is ignored and excluded from duplicate comparison.
    * Updated comment help text.

--- public/psf-devices.ps1
+++ public/psf-devices.ps1
@@ -3,91 +3,111 @@ function Find-FalconDuplicate {
 .SYNOPSIS
 Find potential duplicate hosts within your Falcon environment
 .DESCRIPTION
-If the 'Hosts' parameter is not provided, all Host information will be retrieved. An error will be
-displayed if required fields 'cid', 'device_id', 'first_seen', 'last_seen', 'hostname' and any defined
-'filter' value are not present.
+If 'InputObject' is not provided all host information will be retrieved. If a host does not contain values for
+'cid', 'hostname', 'last_seen', and any specified 'Filter', that host will be excluded from evaluation.
 
-Hosts are grouped by 'cid', 'hostname' and any defined 'filter' values, then sorted by 'last_seen' time. Any
-result other than the one with the most recent 'last_seen' time is considered a duplicate host and is returned
-within the output.
+Hosts are grouped by 'cid', 'hostname' and any defined 'Filter' values. Any result other than the one with the
+most recent 'last_seen' time is considered a duplicate host and is returned within the output.
 
-Hosts can be hidden from the Falcon console by piping the results of 'Find-FalconDuplicate' to
-'Invoke-FalconHostAction' using the action 'hide_host'.
+The list of potential duplicates can be hidden from the Falcon console if provided to 'Invoke-FalconHostAction'
+with the action 'hide_host'.
 
 Requires 'Hosts: Read'.
-.PARAMETER Hosts
-Array of detailed Host results
+.PARAMETER InputObject
+Object array containing detailed host information
 .PARAMETER Filter
-Property to determine duplicates, in addition to 'Hostname'
+One or more additional properties to use to determine duplicates
 .PARAMETER Platform
-Filter hosts by platform
+Filter list of hosts by one or more 'platform_name' values
 .LINK
 https://github.com/crowdstrike/psfalcon/wiki/Find-FalconDuplicate
 #>
   [CmdletBinding(SupportsShouldProcess)]
   param(
-    [Parameter(Position=1)]
-    [object[]]$Hosts,
+    [Parameter(ValueFromPipeline,Position=1)]
+    [Alias('Hosts')]
+    [object[]]$InputObject,
     [Parameter(Position=2)]
     [ValidateSet('external_ip','local_ip','mac_address','os_version','platform_name','serial_number',
       IgnoreCase=$false)]
     [string[]]$Filter,
     [Parameter(Position=3)]
     [ValidateSet('Linux','Mac','Windows',IgnoreCase=$false)]
-    [string]$Platform
+    [string[]]$Platform
   )
   begin {
-    function Group-Selection ($Object,$GroupBy) {
-      ($Object | Group-Object $GroupBy).Where({$_.Count -gt 1 -and $_.Name}).foreach{
-        $_.Group | Sort-Object last_seen | Select-Object -First ($_.Count - 1)
-      }
+    # Required properties for grouping and output
+    [System.Collections.Generic.List[string]]$Criteria = @('cid','hostname')
+    [System.Collections.Generic.List[string]]$Selected = @('cid','device_id','first_seen','last_seen','hostname')
+    if ($Filter) {
+      # Add 'Filter' to required properties for grouping and output
+      $Criteria.Add($Filter)
+      $Selected.Add($Filter)
     }
-    # Comparison criteria and required properties for host results
-    [string[]]$Criteria = 'cid','hostname'
-    [string[]]$Required = 'cid','device_id','first_seen','last_seen','hostname'
-    if ($PSBoundParameters.Filter) {
-      $Criteria = $Criteria + $PSBoundParameters.Filter
-      $Required = $Required + $PSBoundParameters.Filter
+    if ($Platform) {
+      # Add 'platform_name' to output properties when specified
+      $Selected.Add('platform_name')
     }
-    # Create filter for excluding results with empty $Criteria values
-    $FilterScript = "$(($Criteria).foreach{ "`$_.$($_)" } -join ' -and ')"
+    # Create FilterScript for required properties for grouping, with 'last_seen' for sorting
+    $FilterScript = [scriptblock]::Create((@($Criteria + 'last_seen').foreach{
+      '![string]::IsNullOrEmpty($_.{0})' -f $_ } -join ' -and '))
+    [System.Collections.Generic.List[PSCustomObject]]$List = @()
   }
   process {
-    if ($PSCmdlet.ShouldProcess('Find-FalconDuplicate','Get-FalconHost')) {
-      [object[]]$HostArray = if (!$PSBoundParameters.Hosts) {
-        # Retrieve Host details
+    if ($InputObject) {
+      @($InputObject).foreach{
+        # Add filtered pipeline objects to list with required properties
+        if (($Platform -and $Platform -contains $_.platform_name) -or !$Platform) {
+          $i = [PSCustomObject]$_ | Where-Object -FilterScript $FilterScript | Select-Object $Selected
+          if ($i) {
+            $List.Add($i)
+          } else {
+            Write-Log 'Find-FalconDuplicate' ('Ignored host:',([PSCustomObject]$_ |
+              Select-Object $Selected | Format-List | Out-String).Trim() -join "`n")
+          }
+        }
+      }
+    }
+  }
+  end {
+    if (!$List) {
+      if ($PSCmdlet.ShouldProcess('Find-FalconDuplicate','Get-FalconHost')) {
         $Param = @{
-          Field = $Required
+          Field = $Selected
           Detailed = $true
           All = $true
           ErrorAction = 'SilentlyContinue'
         }
-        if ($PSBoundParameters.Platform) {
-          $Param['Filter'] = "platform_name:'$($PSBoundParameters.Platform)'"
-        }
-        Get-FalconHost @Param
-      } else {
-        $PSBoundParameters.Hosts
-      }
-      if ($HostArray) {
-        @($Required).foreach{
-          if (($HostArray | Get-Member -MemberType NoteProperty).Name -notcontains $_) {
-            # Verify required properties are present
-            throw "Missing required property '$_'."
+        if ($Platform) { $Param['Filter'] = "platform_name:[$((@($Platform).foreach{"'$_'"}) -join ',')]" }
+        # Retrieve host details and add filtered hosts to list with required properties
+        @(Get-FalconHost @Param).foreach{
+          $i = [PSCustomObject]$_ | Where-Object -FilterScript $FilterScript
+          if ($i) {
+            $List.Add($i)
+          } else {
+            Write-Log 'Find-FalconDuplicate' ('Ignored host:',([PSCustomObject]$_ |
+              Select-Object $Selected | Format-List | Out-String).Trim() -join "`n")
           }
         }
-        $Param = @{
-          # Group, sort and output result
-          Object = $HostArray | Select-Object $Required | Where-Object -FilterScript {$FilterScript}
-          GroupBy = $Criteria
-        }
-        $Output = Group-Selection @Param
-        if ($Output) {
-          $Output
-        } else {
-          $PSCmdlet.WriteWarning("[Find-FalconDuplicate] No duplicates found.")
-        }
       }
+    }
+    # Output verbose message when grouping starts
+    if ($Platform) {
+      Write-Log 'Find-FalconDuplicate' ('Grouping {0} {1} hosts by {2}' -f $List.Count,($Platform -join ' and '),
+        ((@($Criteria).foreach{"'$_'"}) -join ','))
+    } else {
+      Write-Log 'Find-FalconDuplicate' ('Grouping {0} hosts by {1}' -f $List.Count,((@($Criteria).foreach{
+      "'$_'"}) -join ','))
+    }
+    # Group by 'cid' and 'hostname', then output all but the host with the most recent 'last_seen' value
+    $Output = @($List | Group-Object $Criteria).Where({$_.Count -gt 1 -and $_.Name}).foreach{
+      $_.Group | Sort-Object last_seen | Select-Object -First ($_.Count - 1) | Select-Object $Selected
+    }
+    if ($Output) {
+      Write-Log 'Find-FalconDuplicate' ('Found {0} potential duplicates' -f $Output.Count)
+      $Output
+    } else {
+      $PSCmdlet.WriteWarning('[Find-FalconDuplicate] No duplicates found.')
     }
   }
 }
