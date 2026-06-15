d884bdb6108b54b02fd2a683a56f9f9d65e92658
bk-cs <54042976+bk-cs@users.noreply.github.com>
2026-06-15 13:38:49 -0700

    Update spotlight.ps1
    ### spotlight
    * Removed deprecated endpoints `GET /spotlight/entities/vulnerabilities/v2` and `GET /spotlight/queries/vulnerabilities/v1` from `Get-FalconVulnerability`

--- public/spotlight.ps1
+++ public/spotlight.ps1
@@ -121,65 +121,40 @@ Display total result count instead of results
 .LINK
 https://github.com/crowdstrike/psfalcon/wiki/Get-FalconVulnerability
 #>
-  [CmdletBinding(DefaultParameterSetName='/spotlight/queries/vulnerabilities/v1:get',SupportsShouldProcess)]
+  [CmdletBinding(DefaultParameterSetName='/spotlight/combined/vulnerabilities/v1:get',SupportsShouldProcess)]
   param(
-    [Parameter(ParameterSetName='/spotlight/entities/vulnerabilities/v2:get',Mandatory,
-      ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [ValidatePattern('^[a-fA-F0-9]{32}_[a-fA-F0-9]{32}$')]
-    [Alias('ids')]
-    [string[]]$Id,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get',Mandatory,Position=1)]
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get',Mandatory,Position=1)]
     [ValidateScript({Test-FqlStatement $_})]
     [string]$Filter,
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get',Position=2)]
     [ValidateSet('cve','evaluation_logic','host_info','remediation',IgnoreCase=$false)]
     [string[]]$Facet,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get',Position=3)]
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get',Position=3)]
     [ValidateSet('created_timestamp.asc','created_timestamp.desc','closed_timestamp.asc',
       'closed_timestamp.desc','updated_timestamp.asc','updated_timestamp.desc',IgnoreCase=$false)]
     [string]$Sort,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get',Position=4)]
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get',Position=4)]
     [ValidateRange(1,5000)]
     [int32]$Limit,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get')]
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get')]
     [string]$After,
-    [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get',Mandatory)]
+    [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get')]
     [switch]$Detailed,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get')]
     [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get')]
     [switch]$All,
-    [Parameter(ParameterSetName='/spotlight/queries/vulnerabilities/v1:get')]
+    [Parameter(ParameterSetName='/spotlight/combined/vulnerabilities/v1:get')]
     [switch]$Total
   )
   begin {
     $Param = @{
       Command = $MyInvocation.MyCommand.Name
       Endpoint = $PSCmdlet.ParameterSetName
-      Format = @{ Query = @('after','facet','filter','ids','limit','sort') }
+      Format = @{ Query = @('after','facet','filter','limit','sort') }
     }
-    [System.Collections.Generic.List[string]]$List = @()
-    if ($PSBoundParameters.All -and !$PSBoundParameters.Limit) {
-      # Add maximum limit when not present and using 'All'
-      $PSBoundParameters['Limit'] = 5000
-    }
-    if ($Param.Endpoint -eq '/spotlight/queries/vulnerabilities/v1:get' -and $PSBoundParameters.Limit -gt 400) {
-      # Set maximum limit for /queries/ endpoint
-      $PSBoundParameters['Limit'] = 400
-    }
+    # Add maximum limit when not present and using 'All'
+    if ($PSBoundParameters.All -and !$PSBoundParameters.Limit) { $PSBoundParameters['Limit'] = 5000 }
   }
-  process {
-    if ($Id) { @($Id).foreach{ $List.Add($_) }} else { Invoke-Falcon @Param -UserInput $PSBoundParameters }
-  }
-  end {
-    if ($List) {
-      $PSBoundParameters['Id'] = @($List)
-      Invoke-Falcon @Param -UserInput $PSBoundParameters
-    }
-  }
+  process { Invoke-Falcon @Param -UserInput $PSBoundParameters }
 }
 function Get-FalconVulnerabilityLogic {
 <#
